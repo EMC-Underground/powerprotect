c9b18aba3ac4289c764c95602d416ad698281d4b
Craig Smith <nctiggy@gmail.com>
2021-01-27 13:40:32 -0800

    added version file

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 import setuptools
+import re
+
+
+VERSIONFILE="myniftyapp/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="powerprotect",  # Replace with your own username
-    version="0.1.6",
+    version=verstr,
     author="Brad Soper",
     author_email="bradley.soper@dell.com",
     description="PowerProtect Python Class",
