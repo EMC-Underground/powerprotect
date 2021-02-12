91dae878dd7f673578a05fca081a486e1736f0f7
Craig Smith <nctiggy@gmail.com>
2021-02-11 23:28:43 -0800

    new preotecton rule tests

--- tests/test-protectionrule.py
+++ tests/test-protectionrule.py
@@ -16,6 +16,31 @@ class TestProtectionRule(TestCase):
                      _ProtectionRule__get_protection_rule_by_name(
                          powerprotect.ProtectionRule, "test"))
         self.assertTrue(test_rule.success)
-        print(test_rule.__dict__)
         self.assertDictEqual(test_rule.response, content_example['content'][0])
         self.assertEqual(test_rule.status_code, 200)
+
+    @mock.patch('powerprotect.ppdm.Ppdm._rest_get')
+    def test_get_protection_rule_by_name_not_exists(self, mock_rest_get):
+        content_example = {'content': []}
+        mock_rest_get.return_value.ok = True
+        mock_rest_get.return_value.status_code = 200
+        mock_rest_get.return_value.json.return_value = content_example
+        test_rule = (powerprotect.ProtectionRule.
+                     _ProtectionRule__get_protection_rule_by_name(
+                         powerprotect.ProtectionRule, "test"))
+        self.assertTrue(test_rule.success)
+        self.assertFalse(test_rule.response)
+        self.assertEqual(test_rule.status_code, 200)
+
+    @mock.patch('powerprotect.ppdm.Ppdm._rest_get')
+    def test_get_protection_rule_by_name_fail(self, mock_rest_get):
+        content_example = {'key': 'value'}
+        mock_rest_get.return_value.ok = False
+        mock_rest_get.return_value.status_code = 400
+        mock_rest_get.return_value.json.return_value = content_example
+        test_rule = (powerprotect.ProtectionRule.
+                     _ProtectionRule__get_protection_rule_by_name(
+                         powerprotect.ProtectionRule, "test"))
+        self.assertFalse(test_rule.success)
+        self.assertDictEqual(test_rule.fail_msg, content_example)
+        self.assertEqual(test_rule.status_code, 400)
